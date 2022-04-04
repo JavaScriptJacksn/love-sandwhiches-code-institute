fb02a0a9071f76240aff3f1384b7bae47a3d0243
JavaScriptJacksn <jbjackson247@gmail.com>
2022-04-04 19:13:50 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -23,7 +23,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
@@ -45,8 +45,8 @@ def validate_data(values):
             raise ValueError(
                 f"Exactly 6 values requried, you provided {len(values)}"
             )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
+    except ValueError as error:
+        print(f"Invalid data: {error}, please try again.\n")
         return False
 
     return True
@@ -86,8 +86,8 @@ def calculate_surplus_data(sales_row):
 
 def get_last_5_entries_sales():
     """
-    Collects collumns of data from sales worksheet, collecting 
-    the last 5 entries for each sandwhich and returns the data 
+    Collects collumns of data from sales worksheet, collecting
+    the last 5 entries for each sandwhich and returns the data
     as a list of lists
     """
     sales = SHEET.worksheet("sales")
@@ -99,6 +99,23 @@ def get_last_5_entries_sales():
 
     return columns
 
+
+def calculate_stock_data(data):
+    """
+    Calcualte the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -108,6 +125,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwhiches Data Automation")
